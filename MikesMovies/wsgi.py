adab5b022bb3a876be3b922361cfeadfa0abb491
Michael Gilson <49133101+ZenSorcere@users.noreply.github.com>
2020-03-17 20:05:47 -0700

    initial attempt at uploading beginning files
    First attempt at uploading all files/folders for MikesMovies Project, including the project folder and app folder (MovieReviews).  Github isn't letting me upload all files in one go.

---
+++ MikesMovies/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MikesMovies project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MikesMovies.settings')
+
+application = get_wsgi_application()
